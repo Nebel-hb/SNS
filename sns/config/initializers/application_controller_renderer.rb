81642d348f9c75479ba1abf2c97146003fbfab55
Nebel-hb <nebel.hb@gmail.com>
2021-04-13 13:39:25 +0000

    4/13

---
+++ sns/config/initializers/application_controller_renderer.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# ActiveSupport::Reloader.to_prepare do
+#   ApplicationController.renderer.defaults.merge!(
+#     http_host: 'example.org',
+#     https: false
+#   )
+# end
+
+Refile.secret_key = 'a460a031693549cdde7805f396b604eb136be59fcef7d5c08e0c7406f1a29d3fe8dc44d3c5b6cef6ef97bb14171d1e1d34227af7d2f17518b6273c41e523fbc8'
+Refile.secret_key = '464b660f038c2d158ec80fad4fc34f4ab27d97171ef93bb209b7360b0ce62ef4f6dc316e05ee750fdb91d99de1dbc464541b9c45f6fc115e30776863a004a30a'
+
